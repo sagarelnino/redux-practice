aa67a7e4f1bf26c5b686b75e5e27e498f00d9bec
Shuvashish Paul Sagar <shuvashishpaul64@gmail.com>
2023-02-28 23:12:19 +0900

    2.9 middleware

--- React/src/redux/store.js
+++ React/src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upComingState = [action].reduce(rootReducer, store.getState());
+
+    console.log(`Upcoming state: ${JSON.stringify(upComingState)}`);
+
+    return next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
