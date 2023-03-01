7b0b3d4838c0b5ceb5ae5d0c4b99a7e77918d4b0
Shuvashish Paul Sagar <shuvashishpaul64@gmail.com>
2023-03-01 23:35:33 +0900

    upto 3.5

--- 3/src/App.js
+++ 3/src/App.js
@@ -2,29 +2,33 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
 import Footer from './components/Footer';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <div
-        class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
-    >
-        {/* navbar */}
-        <Navbar />
+    <Provider store={store}>
+      <div
+        className="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
+      >
+          {/* navbar */}
+          <Navbar />
 
-        <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-            {/* header */}
-            <Header />
-            <hr class="mt-4" />
+          <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+              {/* header */}
+              <Header />
+              <hr className="mt-4" />
 
-            {/* todo list */}
-            <TodoList />
+              {/* todo list */}
+              <TodoList />
 
-            <hr class="mt-4" />
+              <hr className="mt-4" />
 
-            {/* footer */}
-            <Footer />
-        </div>
-    </div>
+              {/* footer */}
+              <Footer />
+          </div>
+      </div>
+    </Provider>
   );
 }
 
---
+++ 3/src/redux/todos/initialState.js
@@ -0,0 +1,14 @@
+const initialState = [
+    {
+        id: 1,
+        text: 'Todo Text 1',
+        completed: true
+    },
+    {
+        id: 2,
+        text: 'Todo Text 2',
+        completed: true
+    }
+];
+
+export default initialState;
